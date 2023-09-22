b54c253e886a82856a5bdf8ded50110d3fec4c54
jeannie <jeannie2@users.noreply.github.com>
2023-09-22 09:26:22 +0800

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import FaceApiPage from "./pages/FaceApiPage"
+import Footer from './components/Footer';
 import "./css/main.css"
 
 function App() {
   return (
     <>
       <FaceApiPage />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <a href="https://github.com/jeannie2/faceapi-react">GitHub</a>
+    </div>
+  )
+}
+
+export default Footer
